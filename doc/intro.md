0a1649ed3857b6b96fb2c49155abd7e93a937a95
John <yehohanan7@gmail.com>
2013-02-28 17:46:17 +0000

    project setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wrepl
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/wrepl/core_test.clj
@@ -0,0 +1,7 @@
+(ns wrepl.core-test
+  (:use clojure.test
+        wrepl.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
