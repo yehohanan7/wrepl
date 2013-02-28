88ab319efa988f7b5f8af820389781c38cf18ffd
John <yehohanan7@gmail.com>
2013-02-28 18:12:30 +0000

    initial commit

--- src/wrepl/core.clj
+++ src/wrepl/core.clj
@@ -1,6 +1,34 @@
-(ns wrepl.core)
+(ns wrepl.core
+  (:require [compojure.route :as route]
+            [compojure.core :as compojure]
+            [ring.util.response :as response]
+            [ring.adapter.jetty :as jetty]
+            [clj-json.core :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn eval-clojure [request]
+  (try
+    (let [expr (read-string (slurp (:body request)))]
+      (pr-str (eval expr)))
+    (catch Throwable t
+      (str "ERROR: " t))))
+
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :headers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
+
+(compojure/defroutes app
+  (compojure/POST "/eval" request (eval-clojure request))
+  (compojure/GET "/" request
+    (response/resource-response "public/index.html"))
+  (compojure/GET "/products" [] (json-response [{:id "1" :name "Mac book pro" :price "$22112" :available "ok"}
+                                                {:id "1" :name "Mac book pro" :price "$22112" :available "ok"}
+                                                {:id "1" :name "Mac book pro" :price "$22112" :available "remove"}
+                                                {:id "1" :name "Mac book pro" :price "$22112" :available "ok"}]))
+  (route/resources "/"))
+
+
+(defn -main []
+  (prn "http://localhost:7777/ is active")
+  (jetty/run-jetty app {:join? true :port 3000}))
